bcc7ad8e65b2753cff1db114b71993a93145ff33
Tom Kraak <tom.kraak@gmail.com>
2017-09-19 08:42:41 -0400

    add additional tests and comments

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,43 @@
 import test from 'ava'
-import { getAmount } from '../lib/'
+import { getAmount } from '../lib'
 
-test('getAmount is defined', t => {
+test('getAmount function is defined', t => {
+  // .true(value)
+  // assert that `value` is `true`
   t.true(typeof getAmount === 'function')
+
+  // print a message contextually alongside the test
+  // more useful than console.log
+  t.log('yay, we have a function!')
+})
+
+test('penny should return 1', t => {
+  // .is(value, expected)
+  // assert that `value` is the same as `expected`
+  t.is(getAmount('p'), 1)
+})
+
+test('nickel should return 5', t => {
+  // .not(value)
+  // assert that `value` is not the same as `expected`
+  t.not(!getAmount('n'), 5)
+})
+
+test('dime should return 10', t => {
+  // .truthy(value)
+  // assert that `value` is truthy
+  t.truthy(getAmount('d'), 10)
+})
+
+test('quarter should return 25', t => {
+  // .falsy(value)
+  // assert that `value` is falsy
+  t.falsy(!getAmount('q'), 25)
+})
+
+test('default should throw an error', t => {
+  // .throws(function or promise, error)
+  // assert that `function` throws an error, or `promise` rejects with an error
+  const error = t.throws(() => { getAmount('x') }, Error)
+  t.is(error.message, 'Unrecognized coin x')
 })
